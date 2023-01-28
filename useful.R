8c78375f3add0866c55c3583c735fad2549b6365
Dragonmasterx87 <fahdqadir@gmail.com>
2023-01-27 22:38:56 -0600

    Create useful.R

---
+++ useful.R
@@ -0,0 +1,11 @@
+# Gene ontology analysis
+# Create a list of all files in directory
+dgelist <- gsub("\\.csv$", "", 
+                list.files(r"(C:\Users\mqadir\Box\Lab 2301\1. R_Coding Scripts\Sex Biology Study\Data Output\DE Testing\Windows\New Analysis)", 
+                           all.files = FALSE, 
+                           full.names = FALSE, 
+                           pattern = "*.csv")
+                )
+                
+                
+                
